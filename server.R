cfea2909640a45bf7d36ba53c6028573fe6feb79
Colin Rundel <rundel@gmail.com>
2015-11-04 19:31:31 -0500

    draft

--- server.R
+++ server.R
@@ -1,6 +1,71 @@
+library(truncnorm)
+
 shinyServer(
   function(input, output, session) 
   {
-
+    observe(
+      {
+        updateSliderInput(session,"n_black", max = input$n_drawn)
+      }
+    )
+    
+    priors = reactive(
+      {
+        d_total = numeric()
+        if (input$total_prior == "pois")
+        {
+          d_total = rpois(input$n_sims, input$total_lambda)  
+        } else {
+          d_total = rnbinom(input$n_sims,size = input$total_r, prob = input$total_p)
+        }
+        
+        d_prop = numeric()
+        if (input$prop_prior == "beta")
+        {
+          d_prop = rbeta(input$n_sims, input$prop_alpha, input$prop_beta)  
+        } else {
+          d_prop = rtruncnorm(input$n_sims,0,1,input$prop_mu,input$prop_sigma)
+        }
+        
+        data.frame(total = d_total, prop = d_prop)
+      }
+    )
+    
+    sims = reactive(
+      {
+        gen_model = function(prior_N_total,prior_p_total_black)
+        {
+          n_black_bag = rbinom(1, prior_N_total, prior_p_total_black)
+          p_black_bag = n_black_bag / prior_N_total
+          n_black_hand = rbinom(1,input$n_drawn,p_black_bag)
+          
+          return(n_black_hand)
+        }
+        
+        apply(priors(),1, function(x) gen_model(x[1],x[2]))
+      }
+    )
+    
+    posterior = reactive(
+      {
+        priors()[sims()==input$n_black,]    
+      }
+    )
+    
+    output$total_plot = renderPlot(
+      {
+        par(mar=c(4,4,4,0.1))
+        hist(posterior()[,1], freq=FALSE, main="Total Jelly Beans")
+        lines(density( priors()$total ),col='green',lwd=3)
+      }
+    )
+    
+    output$prop_plot = renderPlot(
+      {
+        par(mar=c(4,4,4,0.1))
+        hist(posterior()[,2], freq=FALSE, main="Proportion of Black Jelly Beans")
+        lines(density( priors()$prop ),col='blue',lwd=3)
+      }
+    )
   }
 )
--- ui.R
+++ ui.R
@@ -1,3 +1,50 @@
+library(shiny)
+
+total_priors = c("Poisson" = "pois", "Negative Binomial" = "nbinom")
+prop_priors = c("Beta" = "beta", "Truncated Normal"="tnorm")
+
 shinyUI(
- 
+  fluidPage(
+    titlePanel(
+      "Jelly Bean ABC"
+    ),
+    sidebarPanel(
+      numericInput("n_sims", h4("Simulations:"),value = 1000, min = 100, step = 1),
+      hr(),
+      h4("Data:"),
+      sliderInput("n_drawn", "Number drawn:", min=1, max=30, value=9, step=1),
+      sliderInput("n_black", "Number black:", min=1, max=30, value=3, step=1),
+      hr(),
+      h4("Priors:"),
+      selectInput("total_prior", "Prior for total", total_priors),
+      selectInput("prop_prior", "Prior for proportion", prop_priors),
+      hr(),
+      h4("Hyperparameters:"),
+      conditionalPanel(
+        condition="input.total_prior == 'pois'",
+        sliderInput("total_lambda",HTML("Total prior - &lambda;"), value = 50, min=1, max=120)
+      ),
+      conditionalPanel(
+        condition="input.total_prior == 'nbinom'",
+        numericInput("total_r",HTML("Total prior - r"), value = 50, min=1, max=120),
+        numericInput("total_p",HTML("Total prior - p"), value = 0.5, min=0, max=1)
+      ),
+      conditionalPanel(
+        condition="input.prop_prior == 'beta'",
+        numericInput("prop_alpha",HTML("Total prior - &alpha;"), value = 1/3 , min=0, max=NA),
+        numericInput("prop_beta",HTML("Total prior - &beta;"), value = 1, min=0, max=NA)
+      ),
+      conditionalPanel(
+        condition="input.prop_prior == 'tnorm'",
+        numericInput("prop_mu",HTML("Proportion prior - &mu;"), value = 0.5, min=NA, max=NA),
+        numericInput("prop_sigma",HTML("Proportion prior - &sigma;"), value = 0.1, min=0, max=NA)
+      )
+    ),
+    mainPanel(
+      h4("Results:"),
+      plotOutput("total_plot"),
+      br(),
+      plotOutput("prop_plot")
+    )
+  )
 )
