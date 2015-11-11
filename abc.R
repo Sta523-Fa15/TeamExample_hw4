814d622f8c94a29a72611dcae9883df399e0bf1b
Colin Rundel <rundel@gmail.com>
2015-11-11 17:39:41 -0500

    Peformance improvements

--- abc.R
+++ abc.R
@@ -1,33 +1,60 @@
-n_sim = 1e5
+library(truncnorm)
+library(parallel)
+library(microbenchmark)
 
-n_black = 3
-n_drawn = 9
-p_black = n_black/n_drawn
 
-# Want posterior for N_total and p_total_black
-
-gen_model = function(prior_N_total,prior_p_total_black)
+f1 = function(n_sim) 
 {
-  n_black_bag = rbinom(1, prior_N_total, prior_p_total_black)
-  p_black_bag = n_black_bag / prior_N_total
-  n_black_hand = rbinom(1,n_drawn,p_black_bag)
-  
-  return(n_black_hand)
+  rtruncnorm(n_sim,0,1,0.25,0.1)
 }
 
-priors = data.frame(
-  prior_N_total = rpois(n_sim, 30),
-  prior_p_total_black = rtruncnorm(n_sim,0,1,0.25,0.1)
-)
+f2 = function(n_sim) 
+{
+  unlist(mclapply(1:10, function (x) rtruncnorm(n_sim/10,0,1,0.25,0.1), mc.cores = 2))
+}
 
-sims = apply(priors,1, function(x) gen_model(x[1],x[2]))
+f3 = function(n_sim) 
+{
+  unlist(mclapply(1:10, function (x) rtruncnorm(n_sim/10,0,1,0.25,0.1), mc.cores = 4))
+}     
 
-posterior = priors[sims==3,]
+f4 = function(n_sim) 
+{
+  unlist(mclapply(1:10, function (x) rtruncnorm(n_sim/10,0,1,0.25,0.1), mc.cores = 8))
+}     
 
-par(mar=c(4,4,4,0.1))
-hist(posterior[,1], freq=FALSE, main="Total Jelly Beans")
-lines(density( priors[,1] ),col='green',lwd=3)
+m = microbenchmark(f1(1e5),f2(1e5),f3(1e5),f4(1e5),times = 10)
 
-par(mar=c(4,4,4,0.1))
-hist(posterior[,2], freq=FALSE, main="Proportion of Black Jelly Beans")
-lines(density( priors[,2] ),col='blue',lwd=3)
+
+
+run_abc = function(n_sim = 1e5)
+{
+  n_black = 3
+  n_drawn = 9
+  p_black = n_black/n_drawn
+  
+  # Want posterior for N_total and p_total_black
+  
+  prior_N_total = rpois(n_sim, 30)
+  if (n_sim < 1e6)
+    prior_p_total_black = rtruncnorm(n_sim,0,1,0.25,0.1)
+  else
+    prior_p_total_black = f4(n_sim)
+  
+  n_black_bag = rbinom(n_sim, prior_N_total, prior_p_total_black)
+  p_black_bag = n_black_bag / prior_N_total
+    
+  n_black_hand = rbinom(n_sim,n_drawn,p_black_bag)
+
+  
+  posterior_N = prior_N_total[n_black_hand==3]
+  posterior_prop = prior_p_total_black[n_black_hand==3]
+  
+  par(mar=c(4,4,4,0.1))
+  hist(posterior_N, freq=FALSE, main="Total Jelly Beans")
+  lines(density( prior_N_total ),col='green',lwd=3)
+  
+  par(mar=c(4,4,4,0.1))
+  hist(posterior_prop, freq=FALSE, main="Proportion of Black Jelly Beans")
+  lines(density( prior_p_total_black ),col='blue',lwd=3)
+}
